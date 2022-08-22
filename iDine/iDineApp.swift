713301880e4b80e114e6262130931f8e13b78568
JoaoGripp <corguinha@gmail.com>
2022-08-22 15:04:58 -0300

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Joao Gripp on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
